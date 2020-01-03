2228ee31499f3dae9967f8bf7792dbedd0757faa
Irfan <irfncr@gmail.com>
2020-01-03 11:50:52 -0600

    initial commit

---
+++ src/main/java/com/irfncr/tacocloud/models/Order.java
@@ -0,0 +1,22 @@
+package com.irfncr.tacocloud.models;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class Order {
+    @NotBlank(message="Name is required")
+    private String name;
+    @NotBlank(message="Street is required")
+    private String street;
+    @NotBlank(message="City is required")
+    private String city;
+    @NotBlank(message="State is required")
+    private String state;
+    @NotBlank(message="Zip code is required")
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+}
