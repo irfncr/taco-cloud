3acfa5b48210c2cdf4d9fd02b1c1106b6230ee0d
Irfan <irfncr@gmail.com>
2020-01-07 19:13:17 -0600

    security is added

---
+++ src/main/java/com/irfncr/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.irfncr.tacocloud.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+    @Override
+    protected  void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery("select username, password, enabled from Users " +
+                "where username=?")
+        .authoritiesByUsernameQuery("select username, authory from UserAuthories " +
+                "where username=?").passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+    }
+}
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -4,22 +4,22 @@ delete from Taco;
 delete from Taco_Order;
 delete from Ingredient;
 insert into Ingredient (id, name, type)
-                values ('FLTO', 'Flour Tortilla', 1);
+                values ('FLTO', 'Flour Tortilla', 0);
 insert into Ingredient (id, name, type)
-                values ('COTO', 'Corn Tortilla', 1);
+                values ('COTO', 'Corn Tortilla', 0);
 insert into Ingredient (id, name, type)
-                values ('GRBF', 'Ground Beef', 2);
+                values ('GRBF', 'Ground Beef', 1);
 insert into Ingredient (id, name, type)
-                values ('CARN', 'Carnitas', 2);
+                values ('CARN', 'Carnitas', 1);
 insert into Ingredient (id, name, type)
-                values ('TMTO', 'Diced Tomatoes', 3);
+                values ('TMTO', 'Diced Tomatoes', 2);
 insert into Ingredient (id, name, type)
-                values ('LETC', 'Lettuce', 3);
+                values ('LETC', 'Lettuce', 2);
 insert into Ingredient (id, name, type)
-                values ('CHED', 'Cheddar', 4);
+                values ('CHED', 'Cheddar', 3);
 insert into Ingredient (id, name, type)
-                values ('JACK', 'Monterrey Jack', 4);
+                values ('JACK', 'Monterrey Jack', 3);
 insert into Ingredient (id, name, type)
-                values ('SLSA', 'Salsa', 5);
+                values ('SLSA', 'Salsa', 4);
 insert into Ingredient (id, name, type)
-                values ('SRCR', 'Sour Cream', 5);
+                values ('SRCR', 'Sour Cream', 4);
