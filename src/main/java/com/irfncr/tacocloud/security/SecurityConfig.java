8720abea6b5369899750d3e75519f51cb328e8c0
Irfan <irfncr@gmail.com>
2020-01-07 23:21:10 -0600

    adding register page

--- src/main/java/com/irfncr/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/irfncr/tacocloud/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,9 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new StandardPasswordEncoder("53cr3t");
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    protected void configure(AuthenticationManagerBuilder auth, HttpSecurity http) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(encoder());
+        http .authorizeRequests()
+                .antMatchers("/design", "/orders") .hasRole("ROLE_USER")
+                .antMatchers("/", "/**").access("permitAll").and()
+                .formLogin().loginPage("/login");
     }
 }
